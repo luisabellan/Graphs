0c27cb400123c26fb03911b9119488725a7d1ba8
Luis <luis.abellan.dev@gmail.com>
2020-08-24 06:35:40 +0200

    graph.py done

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -37,40 +37,72 @@ class Graph:
         beginning from starting_vertex.
         """
         # Create an empty queue and enqueue the starting_vertex 
+        queue = Queue()
+        queue.enqueue({ 
+            'current_vertex': starting_vertex,
+            'path': [starting_vertex]        
+        })
         # Create an empty set to track visited verticies
+        visited_vertices = set()
 
         # while the queue is not empty:
+        while queue.size() > 0:
             # get current vertex (dequeue from queue)
-
+            # get current vertex PATH (dequeue from queue)
+            current_obj = queue.dequeue()
+            current_path = current_obj['path']
+            # set the current vertex to the LAST element of the PATH
+            current_vertex = current_obj['current_vertex']
             # Check if the current vertex has not been visited:
+            if current_vertex not in visited_vertices:
                 # print the current vertex
+                print(current_vertex)
                 # Mark the current vertex as visited
-                    # Add the current vertex to a visited_set
-
+                # Add the current vertex to a visited_set
+                visited_vertices.add(current_vertex)
 
                 # queue up all the current vertex's neighbors (so we can visit them next)
+                for neighbor_vertex in self.get_neighbors(current_vertex):
+                    new_path = list(current_path)
+                    new_path.append(neighbor_vertex)
 
-        pass  # TODO
+                    queue.enqueue({
+                        'current_vertex': neighbor_vertex,
+                        'path': new_path
+                    })
+        return None
+        # pass  # TODO
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        # Create an empty stack and add the starting_vertex 
+        # Create an empty stack and add the starting_vertex
+        stack = Stack()
+        stack.push(starting_vertex) 
         # Create an empty set to track visited verticies
-
+        visited_vertices = set()
         # while the stack is not empty:
+        while stack.size() > 0:
             # get current vertex (pop from stack)
-
+            current_vertex = stack.pop()
             # Check if the current vertex has not been visited:
+            if current_vertex not in visited_vertices:
                 # print the current vertex
+                print(current_vertex)
                 # Mark the current vertex as visited
-                    # Add the current vertex to a visited_set
+                # Add the current vertex to a visited_set
+                visited_vertices.add(current_vertex)
 
 
-                # push up all the current vertex's neighbors (so we can visit them next
-        pass  # TODO
+                # push up all the current vertex's neighbors (so we can visit them next)
+                for neighbor_vertex in self.get_neighbors(current_vertex):
+                    stack.push(neighbor_vertex)
+
+                    stack.push(neighbor_vertex)
+
+        return None
 
     def dft_recursive(self, start_vert, visited=None):
         """
@@ -97,19 +129,19 @@ class Graph:
         """
         # Create an empty queue and enqueue the PATH TO starting_vertex 
         queue = Queue()
-        visited_vertices = set()
-        # Create an empty set to track visited verticies
         queue.enqueue({ 
             'current_vertex': starting_vertex,
             'path': [starting_vertex]        
         })
+        # Create an empty set to track visited verticies
+        visited_vertices = set()
         # while the queue is not empty:
         while queue.size() > 0:
             # get current vertex PATH (dequeue from queue)
             current_obj = queue.dequeue()
             current_path = current_obj['path']
-            current_vertex = current_obj['current_vertex']
             # set the current vertex to the LAST element of the PATH
+            current_vertex = current_obj['current_vertex']
 
             # Check if the current vertex has not been visited:
             if current_vertex not in visited_vertices:
@@ -123,8 +155,8 @@ class Graph:
                 # Add the current vertex to a visited_set
                 visited_vertices.add(current_vertex)
 
-                for neighbor_vertex in self.get_neighbors(current_vertex):
                 # Queue up NEW paths with each neighbor:
+                for neighbor_vertex in self.get_neighbors(current_vertex):
                     new_path = list(current_path)
                     new_path.append(neighbor_vertex)
 
